854206abeab484366a0261c8f591624d0f6daa6c
q15517110516 <lmrzds@gmail.com>
2020-03-21 19:01:20 -0400

    test asynchronous function

--- jest-startup/src/App.js
+++ jest-startup/src/App.js
@@ -1,20 +1,30 @@
+import axios from 'axios';
+import 'babel-polyfill';
 // export default{
 //   sum(a, b){
 //     return a + b;
 //   }
 // }
 
+// export default{
+//   getIntArray(num){
+//     if(!Number.isInteger(num)){
+//       throw Error(' "getIntArray" accepts only integer type parameters ');
+//     }
+
+//     let result = [];
+//     for(let i= 0, len = num; i < len; i++){
+//       result.push(i);
+//     }
+
+//     return result;
+//   }
+// }
+
 export default{
-  getIntArray(num){
-    if(!Number.isInteger(num)){
-      throw Error(' "getIntArray" accepts only integer type parameters ');
-    }
-
-    let result = [];
-    for(let i= 0, len = num; i < len; i++){
-      result.push(i);
-    }
-
-    return result;
+  fetchUser(){
+    return axios.get('http://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.data)
+      .catch(error => console.log(error));
   }
 }
--- jest-startup/src/App.test.js
+++ jest-startup/src/App.test.js
@@ -2,15 +2,32 @@ import App from './App';
 
 // test('sum(2+2) = 4', () => {
 //   expect(App.sum(2,2)).toBe(4);
+// })
+
+// test(' getIntArray(3) should return 3 as the length of the array ', () => {
+//   expect(App.getIntArray(3)).toHaveLength(3);
 // });
 
-test(' getIntArray(3) should return 3 as the length of the array ', () => {
-  expect(App.getIntArray(3)).toHaveLength(3);
-});
+// test(' getIntArray(3.3) should throw an error' , () => {
+//   function getIntArrayWrapFn(){
+//     App.getIntArray(3.3);
+//   }
+//   expect(getIntArrayWrapFn).toThrow(' "getIntArray" accepts only integer type parameters ');
+// })
 
-test(' getIntArray(3.3) should throw an error' , () => {
-  function getIntArrayWrapFn(){
-    App.getIntArray(3.3);
-  }
-  expect(getIntArrayWrapFn).toThrow(' "getIntArray" accepts only integer type parameters ');
-});
+test('fetchUser() could request an object with a name attribute value of Leanne Graham', () => {
+  
+  // This ensures that in an asynchronous test, an assertion will be executed in the callback function.
+  expect.assertions(1);
+
+  return App.fetchUser()
+    .then(data => {
+      expect(data.name).toBe('Leanne Graham');
+    });
+})
+
+test('fetchUser() could request an object with a name attribute value of Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await App.fetchUser();
+  expect(data.name).toBe('Leanne Graham');
+})
